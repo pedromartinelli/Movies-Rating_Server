8e2b8435dc34e2d9f88bf87f615ac2e18749857d
pedro <p.martinellinunes@gmail.com>
2022-09-05 15:20:58 -0300

    criada as funções de busca de filme pelo id, listar os filmes de um usuário por titulo e tag e deletar um filme

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -4,6 +4,9 @@ const MoviesController = require('../controllers/MoviesController');
 const moviesRoutes = Router();
 const moviesController = new MoviesController();
 
+moviesRoutes.get('/', moviesController.index);
 moviesRoutes.post('/:user_id', moviesController.create);
+moviesRoutes.get('/:id', moviesController.show);
+moviesRoutes.delete('/:id', moviesController.delete);
 
 module.exports = moviesRoutes;
