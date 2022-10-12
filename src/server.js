8b6f268ab04c23f322f413ca75f747158e456b71
pedro <p.martinellinunes@gmail.com>
2022-10-12 14:42:06 -0300

    Instalando o Cors e configurando no server.js

--- src/server.js
+++ src/server.js
@@ -2,13 +2,16 @@ require('express-async-errors');
 
 const express = require('express');
 const sqliteConnection = require('./database/sqlite');
-const routes = require('./routes');
 const AppError = require('./utils/AppError');
 const uploadConfig = require('./configs/upload');
+
+const cors = require('cors');
+const routes = require('./routes');
 
 sqliteConnection();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER));
